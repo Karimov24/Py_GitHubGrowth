2dcfc5bc5ac283c0c9b38354310f3747cb5612ee
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-06-30 17:19:02 -0400

    Calculations 2

---
+++ Lesson#5.py
@@ -0,0 +1,9 @@
+# Variables that are assigned numeric values can be treated the same as 
+# the numbers themselves. Two variables can be added together, divided by 2, and multiplied 
+# by a third variable without Python distinguishing between the variables and literals 
+
+quilt_width = 8
+quilt_length = 8
+
+print(quilt_width * quilt_length)
+
