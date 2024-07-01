e5076d2803becbd6044c0d53ef4914a2f5cd48bf
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-07-01 10:06:58 -0400

    Exponents!

---
+++ Lesson#6.py
@@ -0,0 +1,13 @@
+#Python can also perform exponentiation. In written math, you might see an exponent as a superscript number, 
+# but typing superscript numbers isn’t always easy on modern keyboards. Since this operation is so related to multiplication, 
+# we use the notation **.
+
+# Calculation of squares for:
+# 6x6 quilt
+print(6 ** 2 )
+# 7x7 quilt
+print (7 ** 2)
+# 8x8 quilt
+print(8 **2)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+print(6 * 6 * 6 * 6)
