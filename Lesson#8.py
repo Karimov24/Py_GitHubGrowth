89a993486b4a27e61c267b2eceae054bb3b9e140
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-07-02 06:21:19 -0400

    Concatenation

---
+++ Lesson#8.py
@@ -0,0 +1,17 @@
+#Concatenation
+
+#The + operator doesn’t just add two numbers, it can also “add” two strings! 
+# The process of combining two strings is called string concatenation. Performing string concatenation creates a 
+# brand new string comprised of the first string’s contents followed by the second string’s contents
+
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+print(message)
