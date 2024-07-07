bc2e8cff44436a4141e2b7afb0076d832ee9d07a
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-07-06 22:27:58 -0400

    Multi-Strings

---
+++ Lesson#10.py
@@ -0,0 +1,14 @@
+#Multi-line Strings
+
+#ython strings are very flexible, but if we try to create a string that occupies multiple lines 
+# we find ourselves face-to-face with a SyntaxError. Python offers a solution: multi-line strings. 
+# By using three quote-marks (""" or ''') instead of one
+
+# Assign the string here
+to_you = """ 
+Stranger, if you passing meet me and desire to speak to me, why should you not speak to me?
+And why should I not speak to you?
+"""
+
+
+print(to_you)
---
+++ Lesson#7.py
@@ -0,0 +1,16 @@
+#Modulo!!
+#Python offers a companion to the division operator called the modulo operator. The modulo operator is 
+# indicated by % and gives the remainder of a division calculation. If the two numbers are divisible, 
+# then the result of the modulo operation will be 0.
+#----------------------------------------------------
+
+#every 11th customer gets 11% off of their next order.
+order_263_r = 263 % 11
+print(order_263_r)
+
+order_263_coupon = "No"
+
+order_264_r = 264 % 11
+print(order_264_r)
+
+order_264_coupon = "Yes"
