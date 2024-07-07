b88de66d5f652b3a587e9b127f4c5c9a006898d6
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-07-06 22:31:54 -0400

    Delete Lesson#4.py

--- Lesson#4.py
+++
@@ -1,9 +0,0 @@
-#Calculations!!!
-#Computers absolutely excel at performing calculations.
-#Python performs the arithmetic operations of addition, subtraction, multiplication, and division with +, -, *, and /
-
-print(25 * 68 + 13 / 28) 
-
-#The results will be 1700.46428571
-
-
