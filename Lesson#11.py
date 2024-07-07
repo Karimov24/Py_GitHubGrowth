63b2625a51f5dab38ff8f10d2c2557113d73d13a
Rakhmonjon Karimov <karimovrr23@gmail.com>
2024-07-06 22:38:42 -0400

    ASCII Letters

---
+++ Lesson#11.py
@@ -0,0 +1,30 @@
+#Block Letter; The ASCII numbers
+
+#ASCII art is a graphic design technique that uses computers for presentation and consists 
+#of pictures pieced together from individual characters.
+
+#My name is Rakhmonjon Karimov
+#initials R and K
+
+print ("""
+RRRR
+R    R 
+R    R
+RRRR
+R   R
+R    R
+R     R
+""")
+#My name is Rakhmonjon Karimov
+#I want to learn every programming language!!
+
+print("""
+K    K
+K   K
+K  K
+KK
+K  K
+K   K
+K    K
+"""
+)
